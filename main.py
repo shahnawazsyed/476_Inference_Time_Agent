b09ce3f8549982c37d178fc9a99af8ff163d74e8
Shahnawaz Syed <136757049+shahnawazsyed@users.noreply.github.com>
2025-11-11 21:31:11 -0700

    Input data

--- main.py
+++ main.py
@@ -9,3 +9,17 @@ Responsibilities:
 3. Save results (e.g., data/outputs.json).
 4. Ensure efficient execution (<20 API calls per instance).
 """
+
+import json
+
+
+if __name__ == "__main__":
+    main()
+
+def main():
+    input_path = "data/cse476_final_project_dev_data.json"
+    with open(input_path, "r") as f:
+        inputs = json.load(f)
+    for input in inputs:
+        pass
+        #TODO: call agent.py with the prompt
